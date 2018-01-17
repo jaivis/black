653760626ebc5ca8c5495cf75dcbf3e7295e2b40
Aivis @ RB <aivis.jansauskis@live.com>
2018-01-17 14:56:23 +0200

    #removed classificator table and model

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-//            ClassifiersTableSeeder::class,
 //            UsersTableSeeder::class,
 //            DealsTableSeeder::class,
 //            ObjectsTableSeeder::class,
