5bca82f1653b9a83fe8e947b5433cd532aed2fd4
Aivis @ RB <aivis.jansauskis@live.com>
2018-01-29 16:47:05 +0200

    #poga 'atclt filtrus' redzama, ja ir GET[filter]

--- resources/views/deals/filters.blade.php
+++ resources/views/deals/filters.blade.php
@@ -8,8 +8,7 @@
         </h4>
     </div>
     <div class="panel-body">
-{{--        <div id="filtering" class="panel-collapse collapse {{ (isset($_GET['filter'])) ? 'in' : NULL }}">--}}
-        <div id="filtering" class="panel-collapse collapse">
+        <div id="filtering" class="panel-collapse collapse <?= (isset($_GET['filter'])) ? 'in' : NULL; ?>">
 
             <form method="GET" action="{{route('deals.index')}}" id="filter_form">
                 <div class="row">
@@ -27,7 +26,8 @@
                         <div class="form-group">
                             <label for="filter_name">Nosaukums:</label>
                             <input type="text" class="form-control" id="filter_name"
-                                   placeholder="Nosaukums" <?= (isset($_GET['filter']['name']) && $_GET['filter']['name']) ? "value='{$_GET['filter']['name']}' name='filter[name]'" : NULL; ?> v-model="form.name">
+                                   placeholder="Nosaukums"
+                                   <?= (isset($_GET['filter']['name']) && $_GET['filter']['name']) ? "value='{$_GET['filter']['name']}' name='filter[name]'" : NULL; ?> v-model="form.name">
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-2 col-md-1">
@@ -35,14 +35,16 @@
                              v-bind:class="{'has-error': !validation.AMOUNT, 'has-default': validation.AMOUNT}">
                             <label for="filter_amount">Summa:</label>
                             <input type="tel" class="form-control" id="filter_amount"
-                                   placeholder="Summa" <?= (isset($_GET['filter']['amount']) && $_GET['filter']['amount']) ? "value='{$_GET['filter']['amount']}' name='filter[amount]'" : NULL; ?> v-model.number="form.amount">
+                                   placeholder="Summa"
+                                   <?= (isset($_GET['filter']['amount']) && $_GET['filter']['amount']) ? "value='{$_GET['filter']['amount']}' name='filter[amount]'" : NULL; ?> v-model.number="form.amount">
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <div class="form-group">
                             <label for="filter_performer">Izpildītājs:</label>
                             <input type="text" class="form-control" id="filter_performer"
-                                   placeholder="Izpildītājs" <?= (isset($_GET['filter']['performer']) && $_GET['filter']['performer']) ? "value='{$_GET['filter']['performer']}' name='filter[performer]'" : NULL; ?> v-model="form.performer">
+                                   placeholder="Izpildītājs"
+                                   <?= (isset($_GET['filter']['performer']) && $_GET['filter']['performer']) ? "value='{$_GET['filter']['performer']}' name='filter[performer]'" : NULL; ?> v-model="form.performer">
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-6 col-md-3">
@@ -62,7 +64,8 @@
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <div class="form-group">
                             <label for="filter_sections">Iecirkņi:</label>
-                            <select multiple size="10" class="form-control" name="filter[sections_id][]" id="filter_sections"
+                            <select multiple size="10" class="form-control" name="filter[sections_id][]"
+                                    id="filter_sections"
                                     v-model="form.section" :disabled="form.object == ''">
                                 {{--<option value="" selected="selected">-</option>--}}
                                 <option v-for="section in options.sections" v-bind:value="section.ID">
@@ -74,7 +77,8 @@
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <div class="form-group">
                             <label for="filter_elements">Elementi:</label>
-                            <select multiple size="10" class="form-control" name="filter[elements_id][]" id="filter_elements"
+                            <select multiple size="10" class="form-control" name="filter[elements_id][]"
+                                    id="filter_elements"
                                     v-model="form.element">
                                 @foreach(\App\Models\Element::all() as $element)
                                     <option value="{{$element->ID}}">{{$element->NR}} - {{$element->NAME}}</option>
@@ -96,7 +100,8 @@
                     <div class="col-xs-12 col-sm-6 col-md-3">
                         <div class="form-group">
                             <label for="filter_systems">Sistēmas:</label>
-                            <select multiple size="10" class="form-control" name="filter[systems_id][]" id="filter_systems"
+                            <select multiple size="10" class="form-control" name="filter[systems_id][]"
+                                    id="filter_systems"
                                     v-model="form.system" :disabled="(form.type).length == 0">
                                 <option v-for="system in options.systems" v-bind:value="system.ID">
                                     @{{ system.NR }} - @{{ system.NAME }}
@@ -106,12 +111,16 @@
                     </div>
                 </div>
 
-                <a href="{{route('deals.index')}}" class="btn btn-link" style="float: left; color: red;">Atcelt
-                    filtrus</a>
+                @if(isset($_GET['filter']))
+                    <a href="{{route('deals.index')}}" class="btn btn-link" style="float: left; color: red;">Atcelt
+                        filtrus</a>
+                @endif
+
                 <button class="btn btn-link" type="button"
                         v-on:click="resetForm" style="float: left;">
                     Notīrīt laukus
                 </button>
+
                 <button type="submit" class="btn btn-primary" style="float: right;" :disabled="!isValid">Filtrēt
                 </button>
             </form>
